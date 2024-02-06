6b997da1984b710a58dbfd590948cdca6a6b2ecf
largoui <largou.imane.mpsi@gmail.com>
2024-02-06 15:39:00 +0100

    init Flask app

---
+++ app.py
@@ -0,0 +1,11 @@
+import flask
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+   return "<h1>Annuaire Internet</h1><p>Ce site est le prototype d’une API.</p>"
+
+app.run()
